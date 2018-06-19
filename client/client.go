5abc507e865e6c92d3ab03b32730b0525abea6c2
Jarred Parr <jparr721@gmail.com>
2018-06-19 09:25:33 -0400

    remove workspace, add health check for cluster

--- client/client.go
+++ client/client.go
@@ -1,19 +1,21 @@
 package client
 
 import (
-	"github.com/joho/godotenv"
-	"github.com/olivere/elastic"
-	"os"
+	"context"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/olivere/elastic"
 )
 
 // Connector - The elasticsearch client
 type Connector struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 // Load data from .env file into the client
@@ -23,7 +25,7 @@ func (c *Connector) initializeClient() {
 	if err != nil {
 		log.Fatal("Error loading .env file!")
 	}
-	
+
 	c.Host = os.Getenv("HOST")
 	c.Port = os.Getenv("PORT")
 	c.Username = os.Getenv("USERNAME")
@@ -33,14 +35,31 @@ func (c *Connector) initializeClient() {
 func (c *Connector) connect() *elastic.Client {
 	fmt.Println("Connecting to local cluster...")
 	c.initializeClient()
+	connectionString := fmt.Sprintf("http://%s:%s@%s:%s", c.Username, c.Password, c.Host, c.Port)
 
-	connectionString := fmt.Sprintf("http://%s:%s@%s:%s", c.Username, c.Password, c.Host, c.Port) 
 	client, err := elastic.NewClient(elastic.SetURL(connectionString))
-
 	if err != nil {
-		log.Fatal("Error connecting to local elasticsearch cluster!")
+		panic(err)
 	}
 
 	return client
 }
 
+// Checks the status of the cluster
+func (c *Connector) checkHealth() (string, error) {
+	ctx := context.Background()
+
+	client := *c.connect()
+
+	info, code, err := client.Ping(fmt.Sprintf("http://%s:%s", c.Host, c.Port)).Do(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number), nil
+}
+
+// The API functions
+func search() {
+
+}
